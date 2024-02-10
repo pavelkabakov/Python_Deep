b64201c52e242c8b5bfc717653de64ee024004ef
Павел Кабаков <pavelkabakov@gmail.com>
2024-02-11 00:23:15 +0300

    add lesson 6

---
+++ les_6/ex001.py
@@ -0,0 +1,8 @@
+# Скрипт запущен командой: python main.py 10 1010
+
+import random
+from sys import argv
+print(random.uniform(int(argv[1]), int(argv[2])))
+print(random.randrange(int(argv[1]), int(argv[2]), int(argv[1])))
+print(random.sample(range(int(argv[1]), int(argv[2]),
+int(argv[1])), 10))
